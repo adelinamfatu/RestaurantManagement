48599588ddf309b3cf85489a67b1320e4e1ceceb
Adelina Fatu <adelinamfatu@gmail.com>
2023-10-27 17:25:14 +0300

    Added navigation between categories and product panel with back button

--- src/ro/ase/display/MainPage.java
+++ src/ro/ase/display/MainPage.java
@@ -9,7 +9,10 @@ import java.awt.event.ActionListener;
 
 public class MainPage extends JFrame {
     private JLabel titleLabel;
-    private JPanel categoriesPanel;
+    private JPanel mainPanel;
+    private CardLayout cardLayout;
+    private JPanel categoryPanel;
+    private JPanel categoryButtonsPanel;
 
     public MainPage() {
         setTitle(MessageDisplayer.getInstance().getMessage("main_page_title"));
@@ -17,34 +20,61 @@ public class MainPage extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        titleLabel.setText(MessageDisplayer.getInstance().getMessage("main_page_title"));
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        titleLabel = new JLabel(MessageDisplayer.getInstance().getMessage("main_page_title"));
         titleLabel.setFont(new Font("Arial", Font.BOLD, 50));
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
 
         Border spaceBeforeTitle = BorderFactory.createEmptyBorder(20, 0, 10, 0);
         Border spaceAfterTitle = BorderFactory.createEmptyBorder(10, 0, 20, 0);
-
         titleLabel.setBorder(BorderFactory.createCompoundBorder(spaceBeforeTitle, spaceAfterTitle));
 
-        categoriesPanel = new JPanel(new GridLayout(0, 5));
+        categoryPanel = new JPanel(new BorderLayout());
+
+        categoryButtonsPanel = new JPanel(new GridLayout(0, 5));
         Font buttonFont = new Font("Arial", Font.PLAIN, 50);
 
         for (Category category : Category.values()) {
             JButton categoryButton = new JButton(category.toString());
             categoryButton.setFont(buttonFont);
-            categoryButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String categoryText = categoryButton.getText();
-                }
-            });
-
-            categoriesPanel.add(categoryButton);
+            categoryButton.addActionListener(new CategoryButtonActionListener(category.toString()));
+            categoryButtonsPanel.add(categoryButton);
         }
+
+        categoryPanel.add(titleLabel, BorderLayout.NORTH);
+        categoryPanel.add(categoryButtonsPanel, BorderLayout.CENTER);
+
+        mainPanel.add(categoryPanel, "CategoryPanel");
 
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
-        contentPane.add(titleLabel, BorderLayout.NORTH);
-        contentPane.add(categoriesPanel, BorderLayout.CENTER);
+        contentPane.add(mainPanel, BorderLayout.CENTER);
+    }
+
+    private class CategoryButtonActionListener implements ActionListener {
+        private String categoryName;
+
+        public CategoryButtonActionListener(String categoryName) {
+            this.categoryName = categoryName;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JPanel productsPanel = new JPanel();
+
+            JButton backButton = new JButton(MessageDisplayer.getInstance().getMessage("back_to_categories_button"));
+            backButton.setFont(new Font("Arial", Font.PLAIN, 45));
+            backButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    cardLayout.show(mainPanel, "CategoryPanel");
+                }
+            });
+            productsPanel.add(backButton);
+            mainPanel.add(productsPanel, categoryName);
+            cardLayout.show(mainPanel, categoryName);
+        }
     }
 }
